c690ef3e78f3984dd0da5b3d3bc697625c96166d
dangalansky <dangalansky@gmail.com>
2022-06-25 22:37:56 -0600

    Update.

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 import time
+import pandas
+import random
 
-then = time.time()
-input("Type the following and press return: \nHow Now Brown Cow.\n")
-now = time.time()
-print(int(now-then))
+data = pandas
+
+n = random.randint(0, len(type_list)-1)
+type = type_list[n]
+char_count = len(type)
+
+
+
+
+before_time = time.time()
+input(f"Type the following and press return: \n{type}\n")
+after_time = time.time()
+time = int(now-then)/60
+print(f'Shit! took ya {time}sec to type.')
